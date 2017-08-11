899fb838818e8d92553637ef78cc8162b6bb34e6
zhangzengmin <zhangzengmin@cydeng.com>
2017-08-11 16:04:24 +0800

    123123

--- spring-hystrix-test/src/test/java/consul/App.java
+++ spring-hystrix-test/src/test/java/consul/App.java
@@ -1,9 +1,11 @@
 package consul;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.ConsulRawClient;
+import com.ecwid.consul.v1.agent.model.NewService;
 import com.ecwid.consul.v1.agent.model.Service;
   
 public class App {  
@@ -19,5 +21,16 @@ public class App {
         for(Entry<String, Service> en:map.entrySet()){
         	System.out.println(en.getKey()+" "+en.getValue());
         }
+        
+        
+        NewService newService = new NewService();
+        newService.setId("myapp_01");
+        newService.setName("myapp");
+        newService.setTags(Arrays.asList("EU-West", "EU-East"));
+        newService.setPort(8500);
+        newService.setAddress("192.168.1.234");
+        
+        consul.agentServiceRegister(newService);
+        
     }  
 } 
